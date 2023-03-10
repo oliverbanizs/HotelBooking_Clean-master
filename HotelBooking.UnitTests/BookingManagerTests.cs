329014a4dc614a9dd85b587ff8d36255e4aa9be0
oliverbanizs <banizsoliver@gmail.com>
2023-03-10 18:11:01 +0100

    test#3

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -43,56 +43,6 @@ namespace HotelBooking.UnitTests
             Assert.NotEqual(-1, roomId);
         }
 
-        //
-        [Fact]
-        public void FindFullyOccupiedDates_RoomIsFullyBooked_ReturnsExpectedResult()
-        {
-            // Arrange
-            DateTime startDate = new DateTime(2023, 3, 1);
-            DateTime endDate = new DateTime(2023, 3, 10);
-            int roomId = 1;
-
-            // Act
-            List<DateTime> fullyOccupiedDates = bookingManager.GetFullyOccupiedDates(startDate, endDate);
-
-            // Assert
-            Assert.NotNull(fullyOccupiedDates);
-            Assert.Equal(10, fullyOccupiedDates.Count);
-            for (int i = 0; i < fullyOccupiedDates.Count; i++)
-            {
-                Assert.Equal(startDate.AddDays(i), fullyOccupiedDates[i]);
-            }
-        }
-
-        //checks if the GetFullyOccupiedDates method returns a list of fully occupied dates
-        [Fact]
-        public void GetFullyOccupiedRooms_ReturnsExpectedResult()
-        {
-            // Arrange
-            DateTime startDate = DateTime.Today.AddDays(1);
-            DateTime endDate = DateTime.Today.AddDays(4);
-
-            // Act
-            var fullyOccupiedDates = bookingManager.GetFullyOccupiedDates(startDate, endDate);
-
-            // Assert
-            Assert.Equal(new List<DateTime> { startDate, endDate }, fullyOccupiedDates);
-        }
-
-        [Fact]
-        public void GetFullyOccupiedRooms_NoOccupiedDates_ReturnsEmptyList()
-        {
-            // Arrange
-            DateTime startDate = DateTime.Today.AddDays(10);
-            DateTime endDate = DateTime.Today.AddDays(14);
-
-            // Act
-            var fullyOccupiedDates = bookingManager.GetFullyOccupiedDates(startDate, endDate);
-
-            // Assert
-            Assert.Empty(fullyOccupiedDates);
-        }
-
         [Fact]
         public void GetFullyOccupiedRooms_InvalidDates_ThrowsArgumentException()
         {
